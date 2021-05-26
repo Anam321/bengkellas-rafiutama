8e22dbe525baa746dc949f9996faf9a42e208480
anam321 <jackston9@gmail.com>
2021-05-27 01:45:41 +0700

    set template

---
+++ application/views/layout/footer.php
@@ -0,0 +1,113 @@
+<footer class="footer-area">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-sm-6 col-md-4 col-xl-3">
+                <div class="single-footer-widget footer_1">
+                    <a href="index.html"> <img src="<?= base_url(); ?>assets/frontend/img/logo/1.png" alt=""> </a>
+                    <p>So seed seed green that winged cattle in Gahesd thing made fly you're no divided deep move lan Gathering thing us land years living on floor me the cavaty do buty fresh</p>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 col-md-4">
+                <div class="single-footer-widget footer_2">
+                    <h4>Best Services</h4>
+                    <div class="contact_info">
+                        <ul>
+                            <li>
+                                <a href="#">General Contracting</a>
+                            </li>
+                            <li>
+                                <a href="#">Mechanical Engineering</a>
+                            </li>
+                            <li>
+                                <a href="#">Civil Engineering</a>
+                            </li>
+                            <li>
+                                <a href="#">Bridge Construction</a>
+                            </li>
+                            <li>
+                                <a href="#">Electrical Engineering</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 col-md-4">
+                <div class="single-footer-widget footer_2">
+                    <h4>Our Gallery</h4>
+                    <div class="footer_img">
+                        <a href="#"><img src="<?= base_url(); ?>assets/frontend/img/footer_img/footer_1.png" alt=""></a>
+                        <a href="#"><img src="<?= base_url(); ?>assets/frontend/img/footer_img/footer_2.png" alt=""></a>
+                        <a href="#"><img src="<?= base_url(); ?>assets/frontend/img/footer_img/footer_3.png" alt=""></a>
+                        <a href="#"><img src="<?= base_url(); ?>assets/frontend/img/footer_img/footer_4.png" alt=""></a>
+                        <a href="#"><img src="<?= base_url(); ?>assets/frontend/img/footer_img/footer_5.png" alt=""></a>
+                        <a href="#"><img src="<?= base_url(); ?>assets/frontend/img/footer_img/footer_6.png" alt=""></a>
+                        <a href="#"><img src="<?= base_url(); ?>assets/frontend/img/footer_img/footer_7.png" alt=""></a>
+                        <a href="#"><img src="<?= base_url(); ?>assets/frontend/img/footer_img/footer_8.png" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 col-md-4">
+                <div class="single-footer-widget footer_2">
+                    <h4>Contact info</h4>
+                    <div class="contact_info">
+                        <p>4361 Morningview Lane Artland , Street
+                            Latimer, IA 50452 / 23654</p>
+                        <p><span> Address :</span> Hath of it fly signs bear be one blessed after </p>
+                        <p><span> Phone :</span> +2 36 265 (8060)</p>
+                        <p><span> Email : </span>info@colorlib.com </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="copyright_part_text text-center">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <p class="footer-text m-0">
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                Copyright &copy;<script>
+                                    document.write(new Date().getFullYear());
+                                </script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com/" target="_blank">Colorlib</a>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer part end-->
+
+<!-- jquery plugins here-->
+<!-- jquery -->
+<script src="<?= base_url(); ?>assets/frontend/js/jquery-1.12.1.min.js"></script>
+<!-- popper js -->
+<script src="<?= base_url(); ?>assets/frontend/js/popper.min.js"></script>
+<!-- bootstrap js -->
+<script src="<?= base_url(); ?>assets/frontend/js/bootstrap.min.js"></script>
+<!-- easing js -->
+<script src="<?= base_url(); ?>assets/frontend/js/jquery.magnific-popup.js"></script>
+<!-- swiper js -->
+<script src="<?= base_url(); ?>assets/frontend/js/swiper.min.js"></script>
+<!-- isotope js -->
+<script src="<?= base_url(); ?>assets/frontend/js/isotope.pkgd.min.js"></script>
+<!-- particles js -->
+<script src="<?= base_url(); ?>assets/frontend/js/owl.carousel.min.js"></script>
+<script src="<?= base_url(); ?>assets/frontend/js/jquery.nice-select.min.js"></script>
+<!-- swiper js -->
+<script src="<?= base_url(); ?>assets/frontend/js/slick.min.js"></script>
+<script src="<?= base_url(); ?>assets/frontend/js/jquery.counterup.min.js"></script>
+<script src="<?= base_url(); ?>assets/frontend/js/waypoints.min.js"></script>
+<!-- custom js -->
+<script src="<?= base_url(); ?>assets/frontend/js/custom.js"></script>
+</body>
+
+
+<!-- Mirrored from technext.github.io/pexcon/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 25 May 2021 19:23:10 GMT -->
+
+</html>
---
+++ application/views/layout/header.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+
+<!-- Mirrored from technext.github.io/pexcon/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 25 May 2021 19:22:50 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Excon</title>
+    <link rel="icon" href="<?= base_url(); ?>assets/frontend/img/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/frontend/css/bootstrap.min.css">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/frontend/css/animate.css">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/frontend/css/owl.carousel.min.css">
+    <!-- themify CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/frontend/css/themify-icons.css">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/frontend/css/flaticon.css">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/frontend/css/magnific-popup.css">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/frontend/css/slick.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/frontend/css/style.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/frontend/css/font-awesome-ie7.c">
+</head>
+
+<body>
+    <!--::header part start::-->
+    <header class="main_menu home_menu" style="border-bottom: 2px solid #ee390f;">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="index.html"> <img style="width: 190px; height: 60px;" src="<?= base_url(); ?>assets/frontend/img/logo/1.png" alt="logo"> </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="ti-menu"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse main-menu-item justify-content-end" id="navbarSupportedContent">
+                            <ul class="navbar-nav align-items-center">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= base_url('home'); ?>">HOME</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= base_url('about'); ?>">ABOUT</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= base_url('product'); ?>">PRODUCT</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= base_url('blog'); ?>">BLOG</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= base_url('project'); ?>">PROJECT</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= base_url('contact'); ?>">CONTACT</a>
+                                </li>
+                                <li class="d-none d-lg-block">
+                                    <a class="btn_1" href="#">Get a Quote</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
