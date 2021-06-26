d649a6a8c731e67160d066844e8ca3eb63da2ab7
mramdan <mramdan480@gmail.com>
2021-06-26 16:50:31 +0700

    masih kurang

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -70,6 +70,19 @@
         gtag('config', 'G-LDTKQ2NKKJ');
     </script>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-200633772-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-200633772-1');
+    </script>
+
 
     <style>
         .cs-page-loading {
